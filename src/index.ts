301bd7b53fda815ce1cf8f1a0824e8cd0d2da078
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-06-22 12:28:09 -0400

    Desestructuracion de argumentos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
 /*
     ===== Código de TypeScript =====
 */
+
+interface IProduct {
+  desc: string;
+  price: number;
+}
+
+const phone: IProduct = {
+  desc: "Nokia Phone",
+  price: 150,
+};
+
+const tablet: IProduct = {
+  desc: "Ipad Tablet",
+  price: 350,
+};
+
+function calcIVA(products: IProduct[]): [number, number] {
+  let total = 0;
+  //destructuring arguments
+  products.forEach(({ price }) => (total += price));
+  return [total, total * 0.19];
+}
+
+const articles = [phone, tablet];
+
+//destructuring arrays
+const [total, iva] = calcIVA(articles);
+
+console.log("Total articles:", total);
+console.log("IVA articles:", iva);
