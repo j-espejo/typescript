b12a77ca8796ad06d02dd120abf1e1f1ef37e33d
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-06-22 10:06:20 -0400

    01-tipos-basicos

---
+++ src/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nickname: string = "jespejo";
+// se puede establecer tipo number y string
+let hp: number | string = 95;
+let isLife: boolean = false;
+
+hp = "Full";
+
+console.log(nickname, hp);
